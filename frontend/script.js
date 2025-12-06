9679159f1ff8697be675748cc1c9695ffef92a47
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2025-12-07 00:04:56 +0700

    update url

--- frontend/script.js
+++ frontend/script.js
@@ -68,8 +68,8 @@ document.getElementById("next").addEventListener("click", async (e) => {
 
 async function submit(name, char, comment) {
   try {
-    // const response = await fetch("https://entertaiment-week-africa.vercel.app/submit-form", {
-    const response = await fetch("http://localhost:3000/submit-form", {
+    const response = await fetch("https://detty-dec.vercel.app/submit-form", {
+      // const response = await fetch("http://localhost:3000/submit-form", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
